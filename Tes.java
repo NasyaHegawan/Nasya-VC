3f1c938f255776ded4789da43919ed2c0c7dfe29
NasyaHegawan <nasya_putri_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:30:29 +0700

    first commit

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+    public static void main(String[] args) {
+        string nama = "Nasya";
+        string kelas = "X RPL 5";
+        system.out.print("Halo, nama saya" + nasya + "dari kelas" + X RPL 5);
+    }
+}
