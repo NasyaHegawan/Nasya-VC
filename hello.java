854ace70c829f39786ae5e09f4374223b72e0b11
NasyaHegawan <nasya_putri_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:12:04 +0700

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,15 @@
+public class hello {
+    public static void main(String[] args) {
+        String nama="Nasya";
+        String alamat="Tulungagung";
+        int usia=15;
+        char jenisKelamin='P';
+        double beratBadan=45.00;
+
+        System.out.println("Nama: "+nama);
+        System.out.println("Alamat: "+alamat);
+        System.out.println("Usia: "+usia);
+        System.out.println("Jenis Kelamin: "+jenisKelamin);
+        System.out.println("Berat Badan: "+beratBadan);
+    }
+}
